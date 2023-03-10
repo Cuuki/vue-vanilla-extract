3a7d20c6761ae40090da002dd44fc2d4966145e3
Patrick Söllner <patrick@newmonday.co>
2023-03-10 15:36:55 +0100

    feat: add vue-tsc as build step for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), vue()],
+  plugins: [
+    checker({
+      vueTsc: true,
+    }),
+    vanillaExtractPlugin(),
+    vue(),
+  ],
 });
